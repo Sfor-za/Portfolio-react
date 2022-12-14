e0de4b00def62a2dbc8a527c04bdc90a0c2a951d
Aditya <Adityaantonythomas@gmail.com>
2022-12-14 11:59:43 +0530

    added subtitle to the logos with btter alignment

--- src/components/About.js
+++ src/components/About.js
@@ -10,26 +10,27 @@ export default function about() {
       <div className="name about-content mx-3">I'm Adi</div>
       <div className="about-section mx-4">
         I'm an self-taught front-end Dev, currently expanding my knowledge in
-        the front-end world and soon will dabble in Back-end Dev.
+        the front-end world and soon in Back-end too.
         <br />
         <br />
         Physics Enthusiast and Grad studied at Loyola College, Chennai.
         <br />
         <br />
         <br />
+
       </div>
       <div className="tools mx-4">
-        The things I use:
-        <div class="container text-center">
-          <div class="row">
-            <div class="col"><img className="css-image"src={require('./html-logo.png')} alt="html logo"/></div>
-            <div class="col"><img className="css-image" src={require('./css-logo.png')} alt="css logo"/></div>
-            <div class="col"><img className="js-image my-0"src={require('./jsc-logo-2.webp')} alt="js logo"/></div>
+        The things I use :
+        <div class="container text-center ">
+          <div class="row row-spacing justify-content-md-center">
+            <div class="col-md-3"><img className="css-image"src={require('./html-logo.png')} alt="html logo"/><div className="description">HTML</div></div>
+            <div class="col-md-3"><img className="css-image" src={require('./css-logo.png')} alt="css logo"/><div className="description">CSS</div></div>
+            <div class="col-md-3 js-image-container"><img className="js-image mb-n4"src={require('./jsc-logo-2.webp')} alt="js logo"/><div className="js-description">JavaScript</div></div>
           </div>
-          <div class="row">
-            <div class="col"><img className="css-image"src={require('./react-logo.webp')} alt="html logo"/></div>
-            <div class="col"><img className="css-image" src={require('./bootstrap-logo.png')} alt="css logo"/></div>
-            <div class="col"><img className="css-image my-0"src={require('./vercel-logo.png')} alt="js logo"/></div>
+          <div class="row row-spacing justify-content-md-center">
+            <div class="col-md-3"><img className="css-image"src={require('./react-logo.webp')} alt="html logo"/><div className="description">React.js</div></div>
+            <div class="col-md-3"><img className="css-image" src={require('./bootstrap-logo.png')} alt="css logo"/><div className="description">Bootstrap</div></div>
+            <div class="col-md-3"><img className="css-image my-0"src={require('./vercel-logo.png')} alt="js logo"/><div className="description">Vercel</div></div>
           </div>
         </div>
       </div>
